03f76d416f7a5cd58e12493c27c7c9e77dc94675
AbangLZU <shenzb12@lzu.edu.cn>
2017-08-29 19:21:59 +0800

    add function of rmse and Jacobian to tool file. fill the Update and Predict function in the kalman_filter. fill some code to FEKF but not finished. change the code format of whole project.

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,6 +15,32 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+    VectorXd rmse(4);
+    rmse << 0,0,0,0;
+
+    // check the validity of the following inputs:
+    //  * the estimation vector size should not be zero
+    //  * the estimation vector size should equal ground truth vector size
+    if(estimations.size() == 0 || estimations.size() != ground_truth.size()){
+        cout<<"the input is not legal!!!"<<endl;
+        return rmse;
+    }
+
+    //accumulate squared residuals
+    for(int i=0; i < estimations.size(); ++i){
+        VectorXd residual = estimations[i] - ground_truth[i];
+        residual = residual.array() * residual.array();
+        rmse = rmse + residual;
+    }
+
+    //calculate the mean
+    rmse = rmse / estimations.size();
+
+    //calculate the squared root
+    rmse = rmse.array().sqrt();
+
+    //return the result
+    return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
@@ -22,4 +48,30 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   TODO:
     * Calculate a Jacobian here.
   */
+    MatrixXd Hj(3,4);
+    //recover state parameters
+    float px = x_state(0);
+    float py = x_state(1);
+    float vx = x_state(2);
+    float vy = x_state(3);
+
+    //check division by zero
+
+    //compute the Jacobian matrix
+    double pow_addition = px*px + py*py;
+
+    //check division by zero
+    if(fabs(pow_addition) < 0.0001){
+        cout << "CalculateJacobian () - Error - Division by Zero" << endl;
+        return Hj;
+    }
+
+    double temp_x = px/(sqrt(pow_addition));
+    double temp_y = py/(sqrt(pow_addition));
+    Hj << temp_x, temp_y, 0, 0,
+            -py/(pow_addition), px/(pow_addition), 0, 0,
+            py * (vx*py - vy*px)/(pow(pow_addition, 3/2)), px * (vy*px - vx*py)/(pow(pow_addition, 3/2)), temp_x, temp_y;
+
+
+    return Hj;
 }
